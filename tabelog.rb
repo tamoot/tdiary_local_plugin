3bb8aa4201715c8902e7ff7eb98ea3e024e075ca
Masahiro TAMURA <tamoot@gmail.com>
2020-05-06 20:24:27 +0900

    Update tabelog.rb

--- tabelog.rb
+++ tabelog.rb
@@ -2,6 +2,6 @@ require 'uri'
 
 def tabelog_badge(url, name)
    id = File::basename(URI::parse(url).path)
-   script_url = %Q|http://tabelog.com/badge/google_badge?escape=false&rcd=#{id}|
+   script_url = %Q|//tabelog.com/badge/google_badge?escape=false&rcd=#{id}|
    return %Q|<div><strong><a href="#{url}" target="_blank">#{h(name)}</a></strong><br><script src="#{script_url}" type="text/javascript" charset="utf-8"></script></div>|
 end
